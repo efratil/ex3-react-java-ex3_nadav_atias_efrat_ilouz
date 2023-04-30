674028ac9d0df3acb0c9b2289231c1aa499fdeb1
efrat <efratil@edu.hac.ac.il>
2023-04-30 16:58:04 +0300

    add server side

--- react-client/src/components/winState.js
+++ react-client/src/components/winState.js
@@ -1,40 +1,93 @@
-import React, { useState } from "react";
-import { Card, Row, Form, Button, Table, Col } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Card, Row, Form, Button, Table, Col} from "react-bootstrap";
 
-const WinState = ({ guessList }) => {
+const SERVER_URL = "java_react_war/api/highscores";
+const WinState = ({guessList}) => {
     const [name, setName] = useState("");
     const [showHighScoreTable, setShowHighScoreTable] = useState(false);
+    const [highScores, setHighScores] = useState([]);
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+        const fetchData = async () => {
+            // Fetch high scores from server
+            await fetch(SERVER_URL, {
+                method: 'GET',
+                headers: {"Content-Type": "application/json",}
+            })
+                .then((response) => response.json()) // parse response data
+                .then((data) => {
+                    setHighScores(data);
+                });
+        }
+        fetchData().catch(e => {
+            console.log("error");
+        })
+    }, []);
+
+    const handleNameSubmit = (e) => {
         e.preventDefault();
         setName(e.target.elements.name.value);
         setShowHighScoreTable(true);
     };
 
+    const handleScoreSubmit = () => {
+        // Send the user's name and score to the server
+        const data = {username: name, score: guessList.length};
+
+        fetch(SERVER_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => {
+                setShowHighScoreTable(response.ok);
+                if (!response.ok) {
+                    return Promise.resolve(new Error(response.statusText));
+                }
+
+            }).catch(e => {
+            alert(e.message());
+        });
+
+    };
+
     return (
         <Row className={"mb-4 p-3"}>
-            <Card style={{ backgroundColor: "rgba(191, 225, 201, 0.79)" }}>
+            <Card style={{backgroundColor: "rgba(191, 225, 201, 0.79)"}}>
                 <Card.Body>
-                    <Card.Title className={"text-center"}>
-                        You won! your score is: {guessList.length}
-                    </Card.Title>
-                    <Card.Text className={"text-center"}>
-                        You may enter your name below to record your score.
-                    </Card.Text>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="text" name="name" placeholder="Enter your name" required/>
-                        </Form.Group>
-                        <div style={{ marginTop: "10px" }}></div>
-                        <Button type="submit">Submit</Button>
-                    </Form>
-
+                    <>
+                        <Card.Title className={"text-center"}>
+                            You won! your score is: {guessList.length}
+                        </Card.Title>
+                        <Card.Text className={"text-center"}>
+                            You may enter your name below to record your score.
+                        </Card.Text>
+                        {!showHighScoreTable && (
+                            <Form onSubmit={handleNameSubmit}>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Control
+                                        type="text"
+                                        name="name"
+                                        placeholder="Enter your name"
+                                        required
+                                    />
+                                </Form.Group>
+                                <div style={{marginTop: "10px"}}></div>
+                                <Button type="submit">Next</Button>
+                            </Form>
+                        )}
+                    </>
                     {showHighScoreTable && (
-                        <div style={{ marginTop: "10px" }}>
+                        <div style={{marginTop: "10px"}}>
                             <Row>
                                 <Col>
-                                    <Card.Title className={"text-center"} style={{ fontSize: "24px", fontWeight: "bold" }}>
-                                        High Score
+                                    <Card.Title
+                                        className={"text-center"}
+                                        style={{fontSize: "24px", fontWeight: "bold"}}
+                                    >
+                                        High Scores
                                     </Card.Title>
                                     <Table striped bordered hover>
                                         <thead>
@@ -45,13 +98,21 @@ const WinState = ({ guessList }) => {
                                         </tr>
                                         </thead>
                                         <tbody>
+                                        {highScores.map((score, index) => (
+                                            <tr key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{score.name}</td>
+                                                <td>{score.score}</td>
+                                            </tr>
+                                        ))}
                                         <tr>
-                                            <td>1</td>
+                                            <td>{highScores.length + 1}</td>
                                             <td>{name}</td>
                                             <td>{guessList.length}</td>
                                         </tr>
                                         </tbody>
                                     </Table>
+                                    <Button onClick={handleScoreSubmit}>Submit</Button>
                                 </Col>
                             </Row>
                         </div>
@@ -60,6 +121,8 @@ const WinState = ({ guessList }) => {
             </Card>
         </Row>
     );
+
+
 };
 
 export default WinState;
--- src/main/java/hac/javareact/ApiServlet.java
+++ src/main/java/hac/javareact/ApiServlet.java
@@ -1,8 +1,21 @@
 package hac.javareact;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+
+import static java.lang.System.out;
 
 /* You can delete this comment before submission - it's only here to help you get started.
 Your servlet should be available at "/java_react_war/api/highscores"
@@ -13,42 +26,138 @@ fetch("/java_react_war/api/highscores")
 
 @WebServlet(name = "ServletApi", value = "/api/highscores")
 public class ApiServlet extends HttpServlet {
+    private static File file;
+    private static final Logger logger = Logger.getLogger(ApiServlet.class.getName());
+    private static final String SCORES = "scores.dat";
+    private static List<PlayerInfo> playerInfoList = new ArrayList<>();
+    private static String path;
+    private Gson gson = new Gson();
+
     /**
-     *
      * @param request
      * @param response
      * @throws IOException
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        playerInfoList = readHighScoresFromFile();
 
-        // your code here
+        // Return the high scores as a JSON array
+        response.setHeader("Access-Control-Allow-Origin", "*");
 
-        // note: this is necessary to allow cross-origin requests from the React frontend
-        // response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.println("Content type: " + response.getContentType());
 
-        // remove this line ! it's only for you to browse the template
-        response.getWriter().println("You are not supposed to browse this page. It will be used for API calls.");
+        String json = gson.toJson(playerInfoList);
+        PrintWriter out = response.getWriter();
+        out.print(json);
+        out.flush();
     }
 
     /**
-     *
      * @param request
      * @param response
      * @throws IOException
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // your code here
+        try {
+            JsonObject json = JsonParser.parseReader(request.getReader()).getAsJsonObject();
+            response.setHeader("Access-Control-Allow-Origin", "*");
 
-        // note: this is necessary to allow cross-origin requests from the React frontend
-        // response.setHeader("Access-Control-Allow-Origin", "*");
+            String name = json.get("username").getAsString();
+            int score = json.get("score").getAsInt();
 
+            // Add the new player score to the high scores set
+            if (name == null) {
+                response.setStatus(400);
+                response.getWriter().println("Bad request");
+                return;
+            }
+            PlayerInfo newPlayer = new PlayerInfo(name, score);
+            // Write the updated high scores to file
+            writeHighScoresToFile(newPlayer);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().println("Score saved to file successfully");
+        } catch (Exception e) {
+            response.setStatus(500);
+            response.getWriter().println(e.getMessage());
+
+        }
     }
+//
+
 
     @Override
     public void init() {
+        path = getServletContext().getRealPath("/") + File.separator + SCORES;
+        logger.info(path);
+        file = new File(path);
+
     }
+
+    private static List<PlayerInfo> readHighScoresFromFile() throws IOException {
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.err.println("Failed to create high scores file");
+                e.printStackTrace();
+            }
+        }
+        List<PlayerInfo> highScoresList = new ArrayList<>();
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(Files.newInputStream(file.toPath()))) {
+            highScoresList = (List<PlayerInfo>) objectInputStream.readObject();
+            return highScoresList;
+        } catch (EOFException ignored) {
+            return highScoresList;
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Failed to read high scores file");
+            e.printStackTrace();
+            return highScoresList;
+        }
+    }
+
+
+    private static void writeHighScoresToFile(PlayerInfo player) throws IOException {
+        List<PlayerInfo> highScores = readHighScoresFromFile();
+        upadtePlayer(highScores, player);
+
+        List<PlayerInfo> topPlayers = highScores.stream()
+                .sorted(Comparator.comparingInt(PlayerInfo::getScore))
+                .limit(5)
+                .collect(Collectors.toList());
+        // Write the updated high scores to file
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(file.toPath()))) {
+            objectOutputStream.writeObject(topPlayers);
+            objectOutputStream.flush();
+        } catch (IOException e) {
+            System.err.println("Failed to write high scores file");
+            throw new IOException("Failed to write high scores file");
+        }
+    }
+
+
+
+    private static void upadtePlayer(List<PlayerInfo> highScores, PlayerInfo player) {
+        boolean playerExists = false;
+        for (PlayerInfo playerInfo : highScores) {
+            if (Objects.equals(playerInfo.getName(), player.getName())) {
+                playerExists = true;
+                if (playerInfo.getScore() < player.getScore()) {
+                    playerInfo.setScore(player.getScore());
+                }
+                break;
+            }
+        }
+        if (!playerExists) {
+            highScores.add(player);
+        }
+        // highScores.add(player);
+    }
+
 
     @Override
     public void destroy() {
